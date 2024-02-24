2d8cfdaa0a6ce1a55145826905575266a6bdcb93
mary kere <marykere20@gmail.com>
2024-02-24 22:52:39 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# my package
+This library was created as an example of how to publish your own Python package
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='EDSA example python package',
+    long_description=open('README.md').read(),
+    install_requires=['numpy'],
+    url='',
+    author='Mary Kere',
+    author_email='marykere20@gmail.com'
+    
+)
